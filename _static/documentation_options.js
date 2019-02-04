4f9817611c96a0046bba271b9b5c52dd36c0ebee
Nathaniel Graff <nathaniel.graff@sifive.com>
2019-02-04 09:47:26 -0800

    Update docs for metal rename
    Signed-off-by: Nathaniel Graff <nathaniel.graff@sifive.com>

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v0.1.2-76-g7810849',
+    VERSION: 'v0.1.2-92-g9f5c338',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
