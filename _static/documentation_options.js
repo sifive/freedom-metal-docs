87a4b41b74c5f053748f18ff03ae05b2f81fa641
Nathaniel Graff <nathaniel.graff@sifive.com>
2019-02-14 13:46:18 -0800

    Update docs for SPI and ITIM
    Signed-off-by: Nathaniel Graff <nathaniel.graff@sifive.com>

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v0.1.2-92-g9f5c338',
+    VERSION: 'v0.1.2-104-g23d01fd',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
