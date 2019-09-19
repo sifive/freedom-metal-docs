6683ed2c47df5b8e0bf95fe5cfac5491d0747c5c
Nathaniel Graff <nathaniel.graff@sifive.com>
2019-09-19 11:31:48 -0700

    Generate docs for v201908
    Signed-off-by: Nathaniel Graff <nathaniel.graff@sifive.com>

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v201905',
+    VERSION: 'v201908',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
